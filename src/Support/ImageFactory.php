421229744dae91ba7a3e1d40763fee396cb44b11
Lennart Fischer <github@lambdadigamma.com>
2021-10-10 12:30:50 +0200

    Fix

--- src/Support/ImageFactory.php
+++ src/Support/ImageFactory.php
@@ -9,7 +9,7 @@ class ImageFactory
     public static function load(string $path): Image
     {
         return Image::load($path)
-            ->setTemporaryDirectory(config('laravel-medialibrary.temporary_image_directory_path', null))
-            ->useImageDriver(config('media-library.image_driver'));
+            ->useImageDriver(config('media-library.image_driver'))
+            ->setTemporaryDirectory(config('media-library.temporary_image_directory_path', null));
     }
 }
